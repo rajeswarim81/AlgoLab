6ba36e35b9541bf1f934b8d946671cd507a399b5
rajeswarim81 <rajeswarim81@gmail.com>
2017-06-07 11:01:22 +0530

    queue

---
+++ queue.c
@@ -0,0 +1,99 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+typedef struct List{
+	int data;
+	struct List *next;
+}list;
+
+list *head,*tail,*temp;
+
+void insert(int n)
+{
+	temp=(list*)malloc(sizeof(list));
+	temp->data=n;
+	if(head==NULL){
+		head=temp;
+		head->next=NULL;
+		tail=head;
+	}
+
+	else{
+		tail->next=temp;
+		tail=temp;
+		tail->next=NULL;
+    }
+
+}
+
+void delete(){
+	if(head==NULL){
+		return;
+	}
+
+	else if(head==tail){
+		head=NULL;
+		tail=head;
+	}
+	else{
+		head=head->next;
+	}
+}
+
+void traverse(){
+	if(head==NULL){
+		return;
+	}
+	else{
+		temp=head;
+		while(temp!=NULL){
+			if(temp->next==NULL)
+				printf("%d\n",temp->data);
+			
+			else
+			  printf("%d->",temp->data);
+			temp=temp->next;
+		}
+	}
+}
+
+
+main()
+{
+  list *l1;
+  head=NULL;
+  tail=NULL;
+  temp=NULL;
+
+  int n, data;
+
+  while(1){
+
+  	printf("\n1.INSERT\n2.DELETE\n3.TRAVERSE\n4.QUIT\n");
+    scanf("%d",&n);
+
+  switch(n)
+  {
+  	case 1:
+  	  printf("\nEnter data:\t");
+  	  scanf("%d",&data);
+  	  insert(data);
+  	  break;
+
+  	case 2:
+  	  delete();
+  	  break;
+
+  	case 3:
+  	  traverse();
+  	  break;
+
+  	case 4:
+  	  return;
+  }
+}
+  //l1 = newnode(5);
+
+}
